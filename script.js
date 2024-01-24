aa7794f126acffb315fe6aad1910c367383eeff7
Jasur0999 <leaderjasur@gmail.com>
2024-01-24 17:06:53 +0500

    jdy

---
+++ script.js
@@ -0,0 +1,170 @@
+// 1- topshiriq  ///////////////////////////
+
+/*  let a = 5;
+    let b = 3;
+    let c = a * b;
+    console.log(c);
+    let m = 2;
+    let n = 3;
+    let l = 4;
+    let k = 5;
+    console.log(m + n + l + k);  */
+
+//  2- topshiriq  ///////////////////////////////
+
+/*  const PI = 3.14;
+    let K = 5;
+    let L = PI * K;
+    console.log(L);  */
+
+//  3- topshiriq  ////////////////////////////////
+
+/*  let a = 2;
+    let b = a ** 3;
+    let A = 2;
+    let s = 6 * (a ** 2);
+    console.log(b, s);  */
+
+//  4- topshiriq  ///////////////////////////////
+
+/*  let a = 2;
+    let b = 3;
+    let c = 1;
+    let v = a * b * c;
+    let A = 2;
+    let B = 2;
+    let C = 2;
+    let s = 2 * (A * B + B * C + A *C);
+    console.log(v, s);  */
+
+//  5- topshiriq  ///////////////////////////////////////
+
+/*  let a = 5;
+    let b = 7;
+    let c = (a + b)/2;
+    console.log(c);  */
+
+//  6- topshiriq  ////////////////////////////////////////
+
+// let a = 2;
+// let b = 2;
+// let c = Math.sqrt(( a * 2 ) + ( b * 2))
+// console.log(c);
+// let a = 2;
+// let b = 2;
+// let c = 3;
+// let p = a + b + c;
+// console.log(p);
+
+//  7- topshiriq  /////////////////////////////////////////
+
+/*  let r1 = 3;
+    let r2 = 2;
+    let pi = 3.14;
+    let s1 = r1 * pi;
+    let s2 = r2 * pi;
+    let s = (s1 - s2) * pi;
+    console.log(s1, s2, s);  */
+
+//   8- topshiriq  //////////////////////////////////////////
+
+/*  let g = 2;
+    let pi = 3.14;
+    let r = 3;
+    let l = g * pi * r;
+    let s = pi * (r ** 2);
+    console.log(l, s);   */
+
+//  9- tpshiriq  ////////////////////////////////////////////
+
+// let x1 = 2;
+// let x2 = 3;
+// let y1 = 2;
+// let y2 = 3;
+// let l = Math.sqrt((( x1 * 2 ) - ( x2 * 2)) + (( y1 * 2 ) - ( y2 * 2 )));
+// console.log(l);
+
+//  10- topshiriq  ///////////////////////////////////
+
+/*  let a = 2;
+    let b = 3;
+    let c = 4;
+    let d;
+    d = b;
+    b = a;
+    a = c;
+    c = d;
+    console.log(a);
+    console.log(b);
+    console.log(c);  */
+
+//  11- topshiriq  /////////////////////////////////////////
+
+/*  let x = 5; 
+    let a = 3; 
+    let b = 4; 
+    let c = 6; 
+    let d = 7; 
+    let e = 2;
+    let y = b *((x - a) ** c) - d * (( x - a ) ** a) + 2;
+    console.log(y);    */
+
+//  12- topshiriq  /////////////////////////////////////////
+
+// let a = +prompt('Raqam kiriting');
+// let b = a ** a;
+// console.log(++b(b));
+
+//  13- topshiriq  /////////////////////////////////////////
+
+// let TF = +prompt('Son kiriting');
+// let D = ( TF - 32 ) * 5 / 9;
+// console.log(D);
+
+//  14- topshiriq  /////////////////////////////////////////
+
+// let Xkg = 3;
+// let Asom = 5000;
+// let Ykg = 2;
+// let Bsom = 2000;
+// let natija = ( Xkg * Asom) - ( Ykg * Bsom);
+// console.log(natija);
+
+//  15- topshiriq  /////////////////////////////////////////
+
+// let X = 3;
+// let A = 5000;
+// let Y = 2;
+// let B = 2000;
+// let natija = ( X * A) + ( Y * B);
+// console.log( natija);
+
+/////////////   NEXT   ////////////////////////////////////////////
+
+//  1- task  /////////////////////////////////////////
+
+//let abc = +prompt("Uch xonali son kiriting");
+// let d = abc % 10;
+// console.log(d);
+
+// let d = (abc % 100) / 10;
+// console.log(parseInt(d));
+
+
+//  2- task  /////////////////////////////////////////
+
+// let abc = +prompt('Son kiriting');
+// let d = abc / 100;
+// let e = (abc % 100) / 10;
+// let h = abc % 10;
+// let g = d + e + h;
+// console.log(parseInt(g));
+
+
+//  3- task  /////////////////////////////////////////
+
+// let abc = +prompt('Son kiriting')
+// let d = abc / 100;
+// let e = ( abc % 100) / 10;
+// let g = abc % 10;
+// console.log(parseInt(g,e,d));
